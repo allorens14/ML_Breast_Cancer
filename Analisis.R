e77471f7eeefbde97f62d47572592de6be0baff2
Alejandro Llorens <alex.vlc.95@gmail.com>
2020-01-14 16:42:10 +0100

    Añadidos los correlation plots

--- Analisis.R
+++ Analisis.R
@@ -23,9 +23,25 @@ library(VIM)
 aggr(data, prop = FALSE, combined = TRUE, numbers = TRUE, sortVars = TRUE, sortCombs = TRUE)
 
 
-#
+#Ahora vamos a comprobar la relación existente entre las variables. Para ello primero realizo tres "correaliation plot".
+#Uno por cada tipo de dato (mean,se,worst).
 library(psych)
-pairs.panels(data[,c(1:30)], method="pearson",
+pairs.panels(data[,c(2:11)], method="pearson",
              hist.col = "#1fbbfa", density=TRUE, ellipses=TRUE, show.points = TRUE,
              pch=1, lm=TRUE, cex.cor=1, smoother=F, stars = T, main="Cancer Mean")
 
+pairs.panels(data[,c(11:11)], method="pearson",
+             hist.col = "#1fbbfa", density=TRUE, ellipses=TRUE, show.points = TRUE,
+             pch=1, lm=TRUE, cex.cor=1, smoother=F, stars = T, main="Cancer Mean")
+
+pairs.panels(data[,c(2:11)], method="pearson",
+             hist.col = "#1fbbfa", density=TRUE, ellipses=TRUE, show.points = TRUE,
+             pch=1, lm=TRUE, cex.cor=1, smoother=F, stars = T, main="Cancer Mean")
+
+
+
+
+library(GGally)
+ggpairs(data[,c(2:11,1)], aes(color=diagnosis, alpha=0.75), lower=list(continuous="smooth"))+ theme_bw()+
+  labs(title="Cancer Mean")+
+  theme(plot.title=element_text(face='bold',color='black',hjust=0.5,size=12))
